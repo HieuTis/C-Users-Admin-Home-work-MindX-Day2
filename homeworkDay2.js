de7225295ba50f61c7b5db4199ae97bf625c6cab
HieuTis <49943021+HieuTis@users.noreply.github.com>
2019-04-30 20:32:42 +0700

    Add files via upload

---
+++ homeworkDay2.js
@@ -0,0 +1,99 @@
+//Serious exercises 
+//Bài 1: BMI
+// let height = prompt("Chiều cao(cm) của bạn là:");
+// console.log("Chiều cao của bạn là:",height,"cm");
+// let weight = prompt("Cân nặng(kg) của bạn là:");
+// console.log("Cân nặng của bạn là:",weight,"kg");
+
+// let conversion = height/100;
+// console.log("Chiều cao khi đổi sang m là:",conversion,"m");
+
+// let BMI =  weight/(conversion*conversion);
+// console.log("BMI của bạn là:",BMI);
+
+
+// if(BMI<16){
+//     console.log("Severely");
+// }else if(BMI<18.5){
+//     console.log("Underweight");
+// }else if(BMI<25){
+//     console.log("Normal");
+// }else if(BMI<30){
+//     console.log("Overweight");
+// }else{
+//     console.log("Obese");
+// };
+
+
+// //Bài 2:
+// let n = prompt("Số giai thừa bạn cần tính là:");
+// console.log("Giai thừa của",n);
+// let tong =1;
+// for(let i=1;i<=n;i++){
+//     tong = tong*i;
+// };
+// console.log("Kết quả là:",tong);
+
+
+//Turtle exercises
+//Bài1:
+// color("red")
+// for(let i=0; i<4; i++){
+// lt(30)
+// fd(100)
+// rt(60)
+// fd(100)
+// rt(120)
+// fd(100)
+// rt(60)
+// fd(100)
+// lt(30)
+// rt(90)
+// };
+
+//Bài2:
+// color("blue")
+// rt(30)
+// for(let i=0;i<17;i++){
+//   fd(100)
+//  if(i<2){
+//   rt(120)
+//   }
+//  else if(i<7){
+//    rt(72)
+//   }
+
+//   else if(i<11){
+//     color("red")
+//     rt(90)
+//   }
+//  else{
+//     rt(60)
+//   }
+// };
+
+//Study
+//Câu1:
+/*the Boolean datatype is a data type that has one of two possible values(usually denoted true and false), 
+intended to represent the two truth values of logic and Boolean algebra
+Ex:
+boolean(6>7)->>> False
+boolean(6<7)->>> True
+boolean(1+2===4)->>False
+*/
+
+//Câu2:
+// A flow chart is a graphical or symbolic representation of a process.
+
+//Câu3:
+//what is nested conditionals?One conditional can also be nested within another.
+//Ex:
+// If(weather= rain){
+//   console.log("I will stay at home");
+// }else{
+//   if(weather= sunny){
+//     console.log("I will go swimming");
+//   }else(weather= cool){
+//     console.log("I will go drink coffee");
+//   }
+// };
